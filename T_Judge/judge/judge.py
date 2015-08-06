b205ecb1a7ff9aa99e447d4176e59c58c667aee8
Parham Alvani <parham.alvani@gmail.com>
2015-08-06 17:15:25 +0430

    Simple compile function is added.

---
+++ T_Judge/judge/judge.py
@@ -0,0 +1,10 @@
+import subprocess
+
+
+def gcc_compile(input_file: str, output_file: str):
+    try:
+        subprocess.check_output(['gcc', input_file, '-o', output_file])
+    except subprocess.CalledProcessError as exception:
+        print(exception.output)
+    else:
+        print("Compilation was successful")
