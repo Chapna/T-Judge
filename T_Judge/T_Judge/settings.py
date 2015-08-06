a90e0470d609571186d19c12c34a3b3644f39f7d
Parham Alvani <parham.alvani@gmail.com>
2015-08-07 00:07:41 +0430

    markdown handler plugin is added.

--- T_Judge/T_Judge/settings.py
+++ T_Judge/T_Judge/settings.py
@@ -38,10 +38,12 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'markdown_deux',
     'user',
     'news',
     'main',
     'api',
+    'judge',
 )
 
 MIDDLEWARE_CLASSES = (
