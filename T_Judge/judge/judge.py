2c16573e8c655eca4ddf6aaa904b49d19475270c
Parham Alvani <parham.alvani@gmail.com>
2015-08-06 17:27:22 +0430

    Mathlib is added into compile function.

--- T_Judge/judge/judge.py
+++ T_Judge/judge/judge.py
@@ -3,7 +3,7 @@ import subprocess
 
 def gcc_compile(input_file: str, output_file: str):
     try:
-        subprocess.check_output(['gcc', input_file, '-o', output_file])
+        subprocess.check_output(['gcc', input_file, '-o', output_file, '-lm'], stderr=subprocess.STDOUT)
     except subprocess.CalledProcessError as exception:
         print(exception.output)
     else:
