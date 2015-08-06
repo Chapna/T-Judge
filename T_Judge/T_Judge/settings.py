da9aebc987fc1fce41014f8630c54bf3991fcd83
Parham Alvani <parham.alvani@gmail.com>
2015-08-06 22:05:58 +0430

    Judge script improved.

--- T_Judge/T_Judge/settings.py
+++ T_Judge/T_Judge/settings.py
@@ -106,3 +106,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# T_Judge custom setting
+# see README.md for more information
+
+PATH_TO_EASYSANDBOX = ''
--- T_Judge/judge/judge.py
+++ T_Judge/judge/judge.py
@@ -1,10 +1,19 @@
 import subprocess
+import os
+from django.conf import settings
 
 
-def gcc_compile(input_file: str, output_file: str):
+def gcc_compile(source_file: str, executable_file: str):
     try:
-        subprocess.check_output(['gcc', input_file, '-o', output_file, '-lm'], stderr=subprocess.STDOUT)
+        subprocess.check_output(['gcc', source_file, '-o', executable_file, '-lm'], stderr=subprocess.STDOUT)
     except subprocess.CalledProcessError as exception:
         print(exception.output)
     else:
         print("Compilation was successful")
+
+
+def run_and_test(executable_file: str, test_file: str):
+    env = os.environ
+    if settings.PATH_TO_EASYSANDBOX != '':
+        env['LD_PRELOAD'] = settings.PATH_TO_EASYSANDBOX
+    subprocess.check_output([os.path.join('.', executable_file)], env=env)
