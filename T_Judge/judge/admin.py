2edc0f0ed4aac18b59b8916f21e8f04d14aedcc2
Parham Alvani <parham.alvani@gmail.com>
2015-08-06 20:44:22 +0430

    Submit model is added.

--- T_Judge/judge/admin.py
+++ T_Judge/judge/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
+from .models import Submit
+
 # Register your models here.
+admin.site.register(Submit)
--- T_Judge/judge/models.py
+++ T_Judge/judge/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
+
 # Create your models here.
+
+class Submit(models.Model):
+    username = models.CharField()
+    date_uploaded = models.DateField()
+    status = models.CharField(choices=(('Compilation Error', 'ce'),
+                                       ('Wrong Answer', 'wr'),
+                                       ('Runtime Error', 're'),
+                                       ('Time Limit Exceeded', 'tl'))
+                              )
+    source_code = models.FileField(upload_to='source_codes')
