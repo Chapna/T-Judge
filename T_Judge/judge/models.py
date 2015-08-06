7538fb7ae723f7511fa7d15aca621a0e9b77bcca
Parham Alvani <parham.alvani@gmail.com>
2015-08-06 23:00:54 +0430

    Homework, Problem and relations are added.

--- T_Judge/judge/models.py
+++ T_Judge/judge/models.py
@@ -4,11 +4,20 @@ from django.db import models
 # Create your models here.
 
 class Submit(models.Model):
-    username = models.CharField()
-    date_uploaded = models.DateField()
+    username = models.CharField(verbose_name='Username')
+    upload_date = models.DateTimeField(verbose_name='Upload Date', auto_now_add=True)
     status = models.CharField(choices=(('Compilation Error', 'ce'),
                                        ('Wrong Answer', 'wr'),
                                        ('Runtime Error', 're'),
                                        ('Time Limit Exceeded', 'tl'))
-                              )
-    source_code = models.FileField(upload_to='source_codes')
+                              , verbose_name='Status')
+    source_code = models.FileField(upload_to='source_codes', verbose_name='Source Code')
+    problem = models.ForeignKey('Problem')
+
+
+class Problem(models.Model):
+    pass
+
+
+class Homework(models.Model):
+    pass
